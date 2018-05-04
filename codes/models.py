ab3f346b5e429aff87120ec6e4f633a0add67d69
kunal <kunalgk1999@gmail.com>
2018-05-04 23:14:35 +0530

    alpha for beta changes in -- template model

--- codes/models.py
+++ codes/models.py
@@ -13,6 +13,7 @@ class Subject(models.Model):
 
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
+    download_all_link = models.URLField(blank=True)
 
     def __str__(self):
         return self.department.name + " | " + self.name
@@ -20,13 +21,19 @@ class Subject(models.Model):
 
 class Practicle(models.Model):
 
+    language_choices = (
+        ('c', 'c'),
+        ('cpp', 'c++'),
+        ('asm6502', 'assembly'),
+    )
     subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
     aim = models.CharField(max_length=100)
     description = models.CharField(max_length=1000)
     code = models.TextField(blank=True)
     contributor = models.CharField(max_length=100, blank=True)
-    contributor_link = models.URLField(default='www.google.co.in')
-    download_all_link = models.URLField(blank=True)
+    contributor_link = models.URLField(blank=True)
+    language = models.CharField(max_length=10, choices=language_choices, default='c')
+
 
     def __str__(self):
         return self.subject.name + " | " + self.aim
