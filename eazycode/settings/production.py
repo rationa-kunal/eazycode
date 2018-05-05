8f4b9318d0f3adc1186d10d7faf595f127dee836
kunal <kunalgk1999@gmail.com>
2018-05-05 18:42:23 +0530

    beta deploy

--- eazycode/settings/production.py
+++ eazycode/settings/production.py
@@ -11,5 +11,9 @@ DATABASES = {
     )
 }
 
+MIDDLEWARE_CLASSES.append(
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+)
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
